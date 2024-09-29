deb262367668e626b451ffd9f5c3553b4aa3232c
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-09-29 02:09:21 +0000

    11

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,14 @@
 let total = prompt('How many km are left to go?');
 
 // Your code below:
+
+const answer = (total) => {
+    if(parseInt(total) <= 50)
+        return "I'm parking. I'll see you right now";
+    else if(parseInt(total) > 100)
+        return "We still have a bit of driving left to go";
+    else
+        return "We'll be there in 5 minutes";
+}
+
+console.log(answer(total));
