fa063e10421474177987110c49bf60d1e59c3f98
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-09-29 21:03:14 +0000

    13

--- exercises/13-Your_first_switch/app.js
+++ exercises/13-Your_first_switch/app.js
@@ -1,17 +1,23 @@
-function getColor(selection)
-{
-	switch(selection){
+function getColor(selection) {
+	switch (selection) {
 		// Add more options here
-	    default:
-	    	return false;  //returns false because the user picked an unavailable color
-	    break;               
+		case "red":
+			console.log("red");
+			return true;
+		case "green":
+			console.log("green");
+			return true;
+		case "blue":
+			console.log("blue");
+			return true;
+		default:
+			// console.log("False");
+			return false; //returns false because the user picked an unavailable color
 	}
 }
 
-let colorname = prompt('What color do you want?').trim();
+let colorname = prompt("What color do you want?").trim().toLowerCase();
 let isAvailable = getColor(colorname);
 
-if(isAvailable)
-	console.log('Good news! That color is available');
-else 
-	console.log('We are sorry, that color is not available');
+if (isAvailable) console.log("Good news! That color is available");
+else console.log("We are sorry, that color is not available");
