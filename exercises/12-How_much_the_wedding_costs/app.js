de26ac7dd00f2fb4f9b2212ec11a61e8cd7a7bc2
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-09-29 02:18:48 +0000

    12

--- exercises/12-How_much_the_wedding_costs/app.js
+++ exercises/12-How_much_the_wedding_costs/app.js
@@ -1,12 +1,14 @@
-let guests = prompt('How many people are coming to your wedding?');
+let guests = prompt("How many people are coming to your wedding?");
 
-function getPrice(guests){
-    let cost = 0;
-    // Your code here
- 
+function getPrice(guests) {
+	let cost = 0;
+	// Your code here
 
-    return cost;
+	if (parseInt(guests) <= 50) return (cost = 4000);
+	else if (parseInt(guests) <= 100) return (cost = 10000);
+	else if (parseInt(guests) <= 200) return (cost = 15000);
+	else return (cost = 20000);
 }
 
 let price = getPrice(guests);
-console.log('Your wedding will cost '+price+' dollars');
+console.log("Your wedding will cost " + price + " dollars");
