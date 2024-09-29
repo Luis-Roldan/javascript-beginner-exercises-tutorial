893003820bb92f0d0222419bff3c3b7e53cd540e
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-09-29 02:01:49 +0000

    10

--- exercises/10-Create_a_new_function/app.js
+++ exercises/10-Create_a_new_function/app.js
@@ -1,7 +1,7 @@
-function shortIntroduction() {
+function shortIntroduction(name, profession, age) {
   // Complete this function's body and arguments
-  
+  return "hello, my name is" + " " + name + ". I'm a" + " " + profession + " and I'm" + " " + age + " years old"
 }
 
 // Fill the gaps with your data in the correct order
-console.log(shortIntroduction(" ", " ", " "))
+console.log(shortIntroduction("Luis", "Fullstack dev", "30"))
