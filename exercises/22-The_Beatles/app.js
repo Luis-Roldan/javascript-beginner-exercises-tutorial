bb1080751075afbf54a0617275071c78e666a0ff
Luis Fernando Roldan Ortiz <108189366+Luis-Roldan@users.noreply.github.com>
2024-09-29 23:15:59 +0000

    22

--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,4 +1,26 @@
+const sing = () => {
 
+    str = ""
+    for(i = 1; i <= 12; i++){
+        if(i === 5){
+            str += "there will be an answer, "
+            console.log("there will be an answer, ")
+        }else if(i === 11) {
+            str += "whisper words of wisdom, "
+            console.log("whisper words of wisdom, ")
+
+        }else if(i === 12){
+            str += "let it be "
+            console.log("let it be ")
+        } else {
+            str += "let it be, "
+            console.log("let it be, ")
+
+        }
+    };
+    return str;
+
+};
 
 //Your code above ^^^
 
